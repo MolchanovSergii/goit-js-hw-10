62ec4dafb7818c72ce8b00f0f73a784516fd51da
MolchanovSergii <molchanse@gmail.com>
2023-04-22 01:26:14 +0300

    change

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,9 @@
+
 export default function fetchCountries(url) {
   return fetch(url).then(resp => {
     if (!resp.ok) {
-        throw new Error(resp.statusText);
-        
+        throw new Error(resp.statusText);   
     }
-
     return resp.json();
   });
 }
